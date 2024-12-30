21daccce684b1764870782121a0f99a666bea133
harriszk <zacharykeatonharris@gmail.com>
2024-12-30 18:51:19 -0500

    Created basic Spring Boot app

---
+++ backend/src/test/java/com/zachary570/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zachary570.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+    
+}
